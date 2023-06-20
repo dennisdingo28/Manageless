73c0fb9755195afb4a4bb0829e94cc5b72fb5816
Joker 281 <dennismoldovan32@gmail.com>
2023-06-20 19:41:58 +0300

    init setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="logoContainer">
+
+        </div>
+        <div className="linkContainer">
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
