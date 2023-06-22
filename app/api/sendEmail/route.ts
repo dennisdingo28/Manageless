7dba31786abb0032324f134709081fd3357ad45f
Moldovan Dennis <dennismoldovan32@gmail.com>
2023-06-22 12:00:29 +0300

    nodemailer implemented

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -1,31 +1,34 @@
-import { NextRequest, NextResponse } from "next/server";
-import emailjs from "emailjs-com";
-
-function getEmailJSCredentials(){
-    const EMAILJS_SERVICEID=process.env.EMAILJS_SERVICEID;
-    const EMAILJS_TEMPLATEID=process.env.EMAILJS_TEMPLATEID;
-    const EMAILJS_PUBLICKEY=process.env.EMAILJS_PUBLICKEY;
-
-    if(!EMAILJS_SERVICEID || !EMAILJS_TEMPLATEID || !EMAILJS_PUBLICKEY)
-        throw new Error("Sending emails curently have a problem.Please try again later");
-
-    return {EMAILJS_SERVICEID,EMAILJS_TEMPLATEID,EMAILJS_PUBLICKEY};
-}
+import { NextRequest,NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export async function POST(req:NextRequest){
     try{
         const data = await req.json();
+        console.log(data);
         if(Object.keys(data).length===0 || !data)
-            throw new Error("Something went wrong.Please try again later!");
+            throw new Error("Data for sending the email was found empty.Please fill in the inputs.");
+        
+        const transporter = nodemailer.createTransport({
+            service:"gmail",
+            auth:{
+                user:process.env.NODEMAILER_EMAIL,
+                pass:process.env.NODEMAILER_PASSWORD,
+            },
+        });
 
-        emailjs.send(getEmailJSCredentials().EMAILJS_SERVICEID,getEmailJSCredentials().EMAILJS_TEMPLATEID,{
-            from_name:"dingo",
-            to_name:"Dennis",
-            message:"domg",
-        },getEmailJSCredentials().EMAILJS_PUBLICKEY);
+        const mailOptions = {
+            from:process.env.NODEMAILER_EMAIL,
+            to:process.env.NODEMAILER_EMAIL,
+            subject:`New message from ${data.name} - ${data.email}`,
+            html:`<h1>${data.description}</h1>`
+        }
 
-        return NextResponse.json({ok:true,msg:`Email was successfully sent to developer`});
+        await transporter.sendMail(mailOptions);
+
+        return NextResponse.json({ok:true,msg:"Email was successfully sent to the developer"});
+
     }catch(err){
         console.log(err);
+        
     }
 }
