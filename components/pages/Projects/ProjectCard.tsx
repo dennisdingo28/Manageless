5eaedb4469d7e186ad364ce6a9d69b2c006b5b78
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-06-28 13:25:26 +0300

    Core done,to do code refactoring

--- components/pages/Projects/ProjectCard.tsx
+++ components/pages/Projects/ProjectCard.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { MouseEvent } from 'react';
 import { ProjectCardProps } from '@/types'
 import React from 'react'
 import { useState } from 'react';
@@ -16,8 +17,11 @@ const ProjectCard = ({projectTitle,setUser,projectId,deleteProject,setSelectedPr
       <div className='bg-[#1e1e1e] p-4 rounded-md cursor-pointer'>
         <p className='font-[.9em] font-gray-200 font-medium text-center'>Project name: <span className="font-thin tracking-wide">{projectTitle}</span></p>
           <div className='flex items-center justify-center gap-2 mt-2'>
-            <i onClick={()=>setIsOpen(true)} className="bi bi-pencil-square cursor-pointer hover:text-gray-400 duration-75"></i>
-            <i onClick={async ()=>{
+            <i onClick={(e:MouseEvent<HTMLButtonElement>)=>{
+              e.stopPropagation();
+              setIsOpen(true)}} className="bi bi-pencil-square cursor-pointer hover:text-gray-400 duration-75"></i>
+            <i onClick={async (e:MouseEvent<HTMLButtonElement>)=>{
+              e.stopPropagation();
               setProjectErrorMessage("Loading...");
               await deleteProject(`${projectId}`,setProjectErrorMessage)
               
