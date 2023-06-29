d09ab586d66c5862de6733f668f35c0efc07dbb0
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-06-29 19:29:40 +0300

    fixing CORS

--- app/api/getProjectContent/[api_key]/[project_title]/route.ts
+++ app/api/getProjectContent/[api_key]/[project_title]/route.ts
@@ -2,19 +2,13 @@ import { Project } from "@/models/Project";
 import { User } from "@/models/User";
 import { ProjectProps } from "@/types";
 import mongoose from "mongoose";
-import { NextApiRequest } from "next/types";
-import { NextApiResponse } from "next/types";
-import NextCors from "nextjs-cors";
+import { NextRequest,NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest,res: NextApiResponse,{params}:{params:{api_key:string, project_title: string}}){
+export async function GET(req: NextRequest,{params}:{params:{api_key:string, project_title: string}}){
     try{
         console.log(params);
-        await NextCors(req, res, {
-            // Options
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            origin: '*',
-            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-         });
+        const origin = req.headers.get('origin');
+
         if(!params)
             throw new Error('No parameters were provided. Please provide a valid api key and project title. Check our docs.');
         if(!params.api_key)
@@ -28,11 +22,15 @@ export async function GET(req: NextApiRequest,res: NextApiResponse,{params}:{par
         const project = user.projects.find((project: ProjectProps)=>project.projectTitle===params.project_title);
         if(!project)
             throw new Error(`Cannot find any post with project title ${params.project_title}`);
-        return res.json({ok:true,projectContent:project.projectContent});
+        return NextResponse.json({ok:true,projectContent:project.projectContent,status:200, headers: {
+            'Access-Control-Allow-Origin': origin || '*',
+            'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }});
 
     }catch(err){
         if(err instanceof mongoose.Error.CastError)
-            return res.json({ok:false,msg:"Something went wrong while trying to access your data."});
-        return res.json({ok:false,msg:(err as Error).message});
+            return NextResponse.json({ok:false,msg:"Something went wrong while trying to access your data."});
+        return NextResponse.json({ok:false,msg:(err as Error).message});
     }
 }
