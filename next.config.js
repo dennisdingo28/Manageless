f4e1094a475dafac51542169ead6d87707bcfe44
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-06-29 19:47:05 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ const nextConfig = {
                 source: "/api/:path*",
                 headers: [
                   { key: "Access-Control-Allow-Credentials", value: "true" },
-                  { key: "Access-Control-Allow-Origin", value: "*" },
+                  { key: "Access-Control-Allow-Origin", value: "https://manageless.vercel.app" },
                   {
                     key: "Access-Control-Allow-Methods",
                     value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
