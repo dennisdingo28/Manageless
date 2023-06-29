c34bba6d06bb117cfe13506f35ba8dfcfcb9f115
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-06-29 20:29:23 +0300

    APP DONE

--- lib/deleteProjectContent.ts
+++ lib/deleteProjectContent.ts
@@ -7,7 +7,7 @@ export default async function deleteContent(obj:ProjectContentProps,userKey: str
     try{
         if(!obj || Object.keys(obj).length===0)
             throw new Error('No object was provided. Please try again later.');
-        const req = await axios.post(`https://manageless.vercel.app/deleteProjectContent`,{apiKey:userKey,user,object:obj});
+        const req = await axios.post(`https://manageless.vercel.app/api/deleteProjectContent`,{apiKey:userKey,user,object:obj});
         if(req.data.ok)
         {
             const newJsonContent = getObjectProperties(req.data.updatedProject.projectContent);
