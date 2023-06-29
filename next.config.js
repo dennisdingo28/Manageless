0a0c26642e5fc83fff9bf75385525322cba05e86
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-06-29 20:14:05 +0300

    API fix

--- next.config.js
+++ next.config.js
@@ -25,34 +25,35 @@ const nextConfig = {
     async rewrites() {
         return [
           {
-            source: '/api/getProjectContent/:path*',
-            destination: 'https://manageless.vercel.app/api/getProjectContent/:path*', // Replace with your API endpoint
+            source: '/api/getProjectContent/:api_key/:project_title',
+            destination: 'https://manageless.vercel.app/api/getProjectContent/:api_key/:project_title', // Replace with your API endpoint
+          },
+        ];
+    }, 
+    async headers() {
+        return [
+          {
+            source: "/:path*",
+            headers: securityHeaders,
+          },
+          {
+            source: "/api/:path*",
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "*" },
+              {
+                key: "Access-Control-Allow-Methods",
+                value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+              },
+              {
+                key: "Access-Control-Allow-Headers",
+                value:
+                  "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              },
+            ],
           },
         ];
     },
-    async headers(){
-        return [
-            {
-                source:"/:path*",
-                headers:securityHeaders,
-            },{
-                source: "/api/:path*",
-                headers: [
-                  { key: "Access-Control-Allow-Credentials", value: "true" },
-                  { key: "Access-Control-Allow-Origin", value: "https://manageless.vercel.app" },
-                  {
-                    key: "Access-Control-Allow-Methods",
-                    value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-                  },
-                  {
-                    key: "Access-Control-Allow-Headers",
-                    value:
-                      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-                    }
-                ]
-            }
-        ]
-    }
 }
 
 module.exports = nextConfig
